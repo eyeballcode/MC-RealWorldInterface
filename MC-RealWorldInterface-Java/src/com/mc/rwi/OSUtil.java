c0a3cfed26d7fbc793cc54a87089183758bfd7e4
Eyeballcode <edwardyeung39@gmail.com>
2016-11-02 21:09:43 +0800

    DAMMIT REFACTORING WAI U SO SMART??

--- MC-RealWorldInterface-Java/src/com/mc/rwi/OSUtil.java
+++ MC-RealWorldInterface-Java/src/com/mc/rwi/OSUtil.java
@@ -24,9 +24,9 @@ public class OSUtil {
         String os = System.getProperty("os.name").toLowerCase();
         if (os.contains("win"))
             return OS.WINDOWS;
-        else if (os.contains("natives/mac"))
+        else if (os.contains("mac"))
             return OS.MACOSX;
-        else if (os.contains("natives/linux"))
+        else if (os.contains("linux"))
             return OS.LINUX;
         else return OS.WINDOWS;
 
